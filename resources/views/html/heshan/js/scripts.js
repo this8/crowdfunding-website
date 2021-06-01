f3ac176f29ba31dae403471a98332ff3e4c79fd0
wphkweerasinghe <hkalhara777@gmail.com>
2021-06-01 17:11:58 +0530

    modified

--- resources/views/html/heshan/js/scripts.js
+++ resources/views/html/heshan/js/scripts.js
@@ -31,19 +31,19 @@ var per=((amount/bill)*100);*/
 
 var i = 0;
 function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 0;
-    var id = setInterval(frame, 0);
+  if (i==0) {
+    i=1;
+    var elem=document.getElementById("myBar");
+    var width=0;
+    var id=setInterval(frame, 10);
     function frame() {
-      if (width >= 100) {
+      if (width>=100) {
         clearInterval(id);
         i = 0;
       } else {
         width++;
-        elem.style.width = width + "%";
-        elem.innerHTML = width  + "%";
+        elem.style.width=width + "%";
+        elem.innerHTML=width  + "%";
       }
     }
   }
