52c2ece036520f4ae7485f035d2d94c8a9e1bf7d
kalharawimukthi0617 <kalhara19970617@gmail.com>
2021-06-08 22:54:31 +0530

    created model,controller migration for the posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(post $post)
+    {
+        //
+    }
+
+ 
+    public function edit(post $post)
+    {
+        //
+    }
+
+    public function update(Request $request, post $post)
+    {
+        //
+    }
+
+    public function destroy(post $post)
+    {
+        //
+    }
+}
