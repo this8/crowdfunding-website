034daca30cc7cbf1d34d00cdb78ed57dfc1d896f
Thisura Samarakoon <thissamarakoon@gmail.com>
2021-06-10 11:59:49 +0530

    post model created

--- app/Models/post.php
+++ app/Models/post.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class post extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'user_id',
+        'category',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'address',
+        'description',
+        'e-mail',
+        'donation_amount',
+        'required_date',
+        'patient_picture',
+        'med_report_1',
+        'med_report_2',
+        'med_report_3',
+        'med_report_4',
+        'med_report_5',
+    ];
 }
