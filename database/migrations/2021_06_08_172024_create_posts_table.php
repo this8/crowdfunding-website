42a807e06f007426e53028eb4c218ac6985ce4a8
Thisura Samarakoon <thissamarakoon@gmail.com>
2021-06-10 11:47:19 +0530

    database for posts created

--- database/migrations/2021_06_08_172024_create_posts_table.php
+++ database/migrations/2021_06_08_172024_create_posts_table.php
@@ -15,7 +15,24 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('category');
+            $table->string('first name');
+            $table->string('last name');
+            $table->integer('phone number');
+            $table->string('address');
+            $table->string('description');
+            $table->string('e-mail');
+            $table->integer('donation amount');
+            $table->date('required date');
+            $table->string('patient picture');
+            $table->string('med report 1');
+            $table->string('med report 2');
+            $table->string('med report 3');
+            $table->string('med report 4');
+            $table->string('med report 5');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
