1cf648e4e2f7c1bfc807480a4afad5376fa6f1b7
Thisura Samarakoon <thissamarakoon@gmail.com>
2021-06-16 01:43:47 +0530

    Routes created

---
+++ laravel/ufund/resources/views/about.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>uFund</title>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/about.css')}}">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+            crossorigin="anonymous"></script>
+</head>
+
+<body>
+<div class="bg-img-about-us">
+    <div id="about-brand" class="container border border-info rounded-top px-3 py-3 my-4 pt-md-5 pb-md-4 mx-auto text-center bg-light">
+        <h1 class="display-5">About Website name/brand</h1>
+
+        <p class="lead text-muted">
+            Brand name is a <b>Platform for Crowdfunding Medical Bills</b>.<br>
+            The concept here is to fund people who are financially unstable and unable to pay their medical bills, <br>by
+            connecting them with the people who are willing to donate money for their medical bills.<br>
+            <img src="/css/images/about/global-goals.png" alt="Brand Logo" class="py-3">
+        </p>
+
+        <p class="lead text-muted">
+            This project falls under the <b>3rd Goal</b> of <a href="https://sdgs.un.org/goals" target="_blank"
+                                                               rel="noopener noreferrer">Sustainable Development Goals</a>.<br>
+            <img src="/css/images/about/global-goals.png" alt="Sustainable Development Goals image"
+                 class="py-3">
+        </p>
+        <div class="container">
+            <a href="#about-csilluminati">Scroll</a>
+        </div>
+
+
+
+    </div>
+    <div id="about-csilluminati" class="container border border-info px-3 py-4 my-4 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-5">About CSilluminati</h1>
+        <p class="lead text-muted">
+            CSilluminati is a group of <b>N</b>ational <b>S</b>chool of <b>B</b>usiness <b>M</b>anagement Students <br>who
+            are willing to help the society by integrating technology into their lives. <br>
+            CSilluminati provides professional services and product development strategy & prototyping in the area of
+            Web Development. <br>
+            For more information about our other projects visit
+            <a href="www.csilluminati.dev" target="_blank" rel="noopener noreferrer">
+                www.csilluminati.dev
+            </a>
+        </p>
+        <div class="container">
+            <a href="#group-members">Scroll</a>
+        </div>
+
+    </div>
+
+    <div id="group-members" class="container text-center my-4 py-4 mx-auto px-3 border border-info rounded-bottom">
+        <h1 class="display-5">Group Members</h1>
+
+        <div class="container text-center my-4 py-4 mx-auto px-3">
+            <div class="row">
+                <div class="col border rounded">
+                    <span>Afraj</span>
+                </div>
+                <div class="col border rounded">
+                    <span>Heshan</span>
+                </div>
+                <div class="col border rounded">
+                    <span>Kalhara</span>
+                </div>
+                <div class="col border rounded">
+                    <span>Sasanka</span>
+                </div>
+                <div class="col border rounded">
+                    <span>Thisura</span>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <a href="#about-brand">Go to top</a>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+</html>
