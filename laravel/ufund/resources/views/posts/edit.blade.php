5ae4a1fb8c074a0d8f6a3b2e7a62d475ffadf003
kalharawimukthi0617 <kalhara19970617@gmail.com>
2021-07-12 18:19:04 +0530

    add bank details to editing post

--- laravel/ufund/resources/views/posts/edit.blade.php
+++ laravel/ufund/resources/views/posts/edit.blade.php
@@ -17,127 +17,136 @@
 
 <body>
 
-    @include('navbar')
+    @include('navbar');
 
 <!-- creating post -->
 
 <div class="container">
-    <div class="row">
-        <div class="col-lg-3">
-
-        </div>
-
-        <div class="col-lg-6">
-            <div id="ui">
-                <form class="form-group text-center" method="POST" action="/posts/{{$post->id}}" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                <center>
-                    <h1>Creating Posts</h1>
-                </center>
-
-                <div class="row">
-
-                    <div class="col-lg-12">
-                        <label> Category </label>
-                        <select class="form-control" name="categoryId">
-                            @foreach ($categories as $category)
-                                    <option value="{{$category->id}}">{{$category->name}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="col-lg-12">
-                        <br>
-                        <br> <br>
-
-                <div class="col-lg-12">
-
-                    <label>Age</label>
-                    <input type="number" name="age" class="form-control" placeholder="Enter your Age">
-
-                         <br>
+        <form class="form-group text-center" method="POST" action="/posts/{{$post->id}}" enctype="multipart/form-data">
+            
+            @csrf
+            @method('PUT')
+                <div id="ui">
+                    <center>
+                        <h1 style="color: red">Creating Post</h1>
+                    </center>
+                    <div class="row">
+                        <div class="col-lg-6">
+        
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <label> Category </label>
+                                    <select class="form-control round_input" name="categoryId">
+                                        
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}"> {{ $category->name }} </option>
+                                        @endforeach
+            
+                                    </select>
+                                </div>
+            
+                                <br> <br>
+            
+                                <div class="col-lg-12">
+            
+                                        <label>Age</label>
+                                        <input type="number" name="age" class="form-control round_input"  value="{{$post->Age}}">
+            
+                                        <br>
+                                </div>
+            
+                                <div class="col-lg-12">
+                                    <br>
+            
+                                    <div class="alert alert-danger" style="border-radius: 20px; " role="alert">
+                                        <a  class="alert-link">If patient is less than 16 years old,<br> Enter parent's NIC number</a>.
+                                    </div>
+                                    <label>Enter Your NIC number</label>
+                                    <input type="text" name="nic" class="form-control round_input" value="{{$post->NIC_number}}">
+                                </div>
+            
+                                <div class="col-lg-12">
+                                    <br>
+                                    <label>Description About patient</label>
+            
+                                    <textarea class="form-control round_textarea" cols="30" rows="4" name="description">{{$post->description}}</textarea>
+                                    <br>
+                                </div>
+            
+                                <div class="col-lg-12">
+                                    <label>Address</label>
+            
+                                    <textarea class="form-control round_textarea" cols="30" rows="4" name="address">{{$post->address}}</textarea>
+                                    <br>
+                                </div>
+            
+                            </div>
+                       
                         </div>
+                        <div class="col-lg-6">
+                            <div class="row">    
+                                <div class="col-lg-12">
+                                    <label>Phone Number</label>
+                                    <input type="number" name="phoneNumber" class="form-control round_input" value="{{$post->phone_number}}">
+            
+                                    <br>
+                                </div>                            
+            
+                                <div class="col-lg-6">
+                                    <label>Required Donation Amount</label>
+            
+                                    <input type="number" name="rqAmount"  class="form-control round_input" value="{{$post->donation_amount}}">
+            
+                                    <br>
+                                </div>
+            
+                                <div class="col-lg-6">
+                                    <label>Required date</label>
+            
+                                    <input type="date" name="date" class="form-control round_input"  value="{{$post->required_date}}">
+                                    <br>
+                                </div>            
+                                
+                                <div class="col-lg-12">
+            
+                                    <label>Bank Name</label>
+                                    <input type="text" name="bankName" class="form-control round_input"  value="{{$post->bank_name}}">
+        
+                                    <br>
+                                </div>
 
-                        <div class="alert alert-danger" role="alert">
-                            <a  class="alert-link">If patient is less than 16 years old,<br> Enter parent's NIC number</a>.
+                                <div class="col-lg-12">
+            
+                                    <label>Bank Account Number</label>
+                                    <input type="text" name="acNum" class="form-control round_input" value="{{$post->bank_account_no}}">
+        
+                                    <br>
+                                </div>
+
+                                <div class="col-lg-12">
+            
+                                    <label>Branch Name</label>
+                                    <input type="text" name="brName" class="form-control round_input" value="{{$post->branch_name}}">
+    
+                                    <br>
+                                </div>
+
+                            </div>
+
+                            <br>
+                          
                         </div>
-                        <label>Enter Your NIC number</label>
-                        <input type="text" name="nic" class="form-control round_input" placeholder="Enter your NIC number." value="{{$post->NIC_number}}">
-                    </div>
-
-                    <div class="col-lg-12">
                         <br>
-                        <label>Description About patient</label>
-
-                        <textarea class="form-control round_textarea" cols="30" rows="4" name="description">{{$post->description}}</textarea>
-                        <br>
+                        
                     </div>
-
-                    <div class="col-lg-12">
-                        <label>Phone number</label>
-                        <input type="number" name="phoneNumber" class="form-control round_input" placeholder="Enter your phone number.." value="{{$post->phone_number}}">
-
-                        <br>
-                    </div>
-
-                    <div class="col-lg-12">
-                        <label>Address</label>
-
-                        <textarea class="form-control round_textarea" cols="30" rows="4" name="address" >{{$post->address}}</textarea>
-                        <br>
-                    </div>
-
-                    {{-- <div class="col-lg-12">
-
-                      <label>E-mail</label>
-                      <input type="email" name="email" class="form-control" placeholder="Enter your email..">
-
-                      <br>
-                    </div> --}}
-
-                    <div class="col-lg-6">
-                        <label>Required Donation Amount</label>
-
-                        <input type="number" name="rqAmount"  class="form-control round_input" placeholder="Enter donation amount.." value="{{$post->donation_amount}}">
-
-                        <br>
-                    </div>
-
-                    <div class="col-lg-6">
-                        <label>Required date</label>
-
-                        <input type="date" name="date" class="form-control round_input" placeholder="Enter required date.." value="{{$post->required_date}}">
-                        <br>
-                    </div>
-
-
-                    {{-- <div class="inputUpload">
-                        <label for="file-ip" class="fileLabel">upload patient picture</label>
-
-                        <input type="file" name="patient_pic"  class="uploadFile" accept="image/*">
-
-                        <br><br>
-                        @csrf
-                        <label for="file-ip"  class="fileLabel">upload medical report</label>
-
-                        <input type="file" name="med_repo"  class="uploadFile">
-
-                        <br><br>
-                    </div> --}}
-
+                    
+                    <center>
+                        <input type="submit" style="width:80%;" name="Submit" value="Update" class="btn btn-primary btn-block long_button">
+                        <input type="reset" style="width:80%;" name="reset" value="Reset" class="btn btn-danger btn-block long_button">
+                    </center>
+                    
                 </div>
-                <input type="submit" name="Submit" value="Update" class="btn btn-primary btn-block long_button">
-                <input type="reset" name="reset" value="Reset" class="btn btn-danger btn-block long_button">
-
-                </form>
-            </div>
-        </div>
-
-        <div class="col-lg-3">
-
-        </div>
-    </div>
+        </form>
 </div>
 <!-- creating post end-->
 
@@ -145,7 +154,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-
+<script src="{{asset('js/form.js')}}"></script>
 </body>
 
 </html>
