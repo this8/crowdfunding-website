ce332acfc612749868ca972331a41f8ce63fe94b
kalharawimukthi0617 <kalhara19970617@gmail.com>
2021-07-15 23:55:42 +0530

    update login page,register page

--- laravel/ufund/resources/views/auth/register.blade.php
+++ laravel/ufund/resources/views/auth/register.blade.php
@@ -1,59 +1,52 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+@extends('layouts.auth-front-end')
+
+@section('content')
+
+<center>
+    <form class="my-form shadow" method="POST" action="{{ route('register') }}">
+        @csrf
+    
+        <!-- Name -->
+        <div>
+    
+            <x-input id="name" placeholder="Enter patient name" class=" inputs block mt-1 w-full  form-control" type="text" name="name" :value="old('name')" required autofocus />
+        </div>
+    
+        <!-- Email Address -->
+        <div class="mt-4">
+    
+            <x-input id="email" placeholder="Enter email" class="inputs block mt-1 w-full form-control" type="email" name="email" :value="old('email')" required />
+        </div>
+    
+        <!-- Password -->
+        <div class="mt-4">
+    
+            <x-input id="password" placeholder="Enter password" class="inputs block mt-1 w-full form-control"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+        </div>
+    
+        <!-- Confirm Password -->
+        <div class="mt-4">
+    
+            <x-input id="password_confirmation" placeholder="Re-Enter password" class="inputs block mt-1 w-full form-control"
+                            type="password"
+                            name="password_confirmation" required />
+        </div>
+    
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
             </a>
-        </x-slot>
+    
+            <br><br>
+            <x-button class="btn btn-danger">
+                {{ __('Register') }}
+            </x-button>
+        </div>
+    </form>
+</center>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@endsection
---
+++ laravel/ufund/resources/views/layouts/auth-front-end.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{asset('css/auth.css')}}">
+
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <br><br>
+                        <h1 class="display-3"> <b> uFund</b></h1>
+                    </div>
+                    <div class="col-lg-8">
+                        <br><br>
+                        <img src="css/images/ufund.jpeg" width="90px" alt="uFund logo" style="margin-top: 10px">
+
+                    </div>
+                </div>
+                
+                <p> Crowdfunding website for medical bills.</p> 
+            </div>
+
+            <div class="col-lg-6">
+                @yield('content')
+            </div>
+        </div>
+        
+    </div>
+    
+</body>
+</html>
