e15906c7da76d43fadeddf5243acc04cdec44377
kalharawimukthi0617 <kalhara19970617@gmail.com>
2021-07-16 09:03:26 +0530

    convert show.html file of profile-page branch to show.blade.php, update show.blade.php file and adde it to the wev site

--- laravel/ufund/resources/views/posts/show.blade.php
+++ laravel/ufund/resources/views/posts/show.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="page.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{asset('css/show.css')}}">
+
     <title>Poster</title>
 </head>
 <body>
@@ -14,66 +16,90 @@
   @include('navbar')
 
       <!-- Poster -->
-
-      <div class="card-deck">
-
-        <div class="card col-12">
-          <div class="col-image">
-            <img class="card-img-left img-responsive" style="width: 500px" src="{{ asset('patients/' . $post->NIC_number . '/patient-profile-pic/' . $post->patient_picture) }}" alt="Card image cap">
-            <h4 class="card-name ">{{$post->Category->name}}</h4>
-          </div>
-          <div class="card-body">
-            <!-- Components -->
-
-
-            <h6 class="card-body-title">NIC number :{{$post->NIC_number}}</h6>
-            <table class="card-items">
-              <tr>
-                <td>description :</td>
-              </tr>
-              <tr>
-                  <td></td>
-                  <td>
-                      &nbsp;<textarea cols="30" rows="10">{{$post->description}}</textarea>
-
-                  </td>
-              </tr>
-              <tr>
-                <td>Phone number:</td>
-                <td>{{$post->phone_number}}</td>
-              </tr>
-              <tr>
-                <td>Address:</td>
-                <td>{{$post->address}}</td>
-              </tr>
-              <tr class="table-danger">
-                <td >Required amount:</td>
-                <td>{{$post->donation_amount}}</td>
-              </tr>
-              <tr>
-                <td>Required date:</td>
-                <td>{{$post->required_date}}</td>
-              </tr>
-            </table>
-            <br>
-            <!-- progress bar -->
-            <div class="w3-light-grey w3-round-xlarge">
-              <div class="w3-container w3-blue w3-round-xlarge" style="width: 50%;">50%</div>
-            </div>
-
+        {{-- <div class=""> --}}
+          <center>
+          <div class="body-border">
             
-          </div>
-        </div>
-    </div><br><br>
+              <div class="col-image" style="margin-top: 30px">
+                <img class="profileImage col-md-12" style="width: 200px; height:200px;" src="{{ asset('patients/' . $post->NIC_number . '/patient-profile-pic/' . $post->patient_picture) }}" alt="Card image cap">
+                <br><br>
+                <h4 class="card-name display-4" style="color: red"><b> {{$post->user->name}}</b> </h4>
+              </div>
+            
+              <br><br>
+              <p style="">
+                {{$post->description}}
+              </p>
 
-     <a href="/posts/{{$post->id}}/edit" class="btn btn-outline-primary">edit</a>
-     <br><br>
-     <form action="/posts/{{$post->id}}" method="POST">
-            @csrf
-            @method('delete')
-              <button class="btn btn-outline-danger" onclick="return confirm('Are you sure ?');" > Delete </button>
-        </form>   
+              <br>
+              <div class="row">
+
+                <div class="col-xl-3">
+
+                </div>
+
+                <div class="col-xl-3">
+                  <a href="/posts/{{$post->id}}/edit" class="btn btn-outline-primary" style="margin-bottom: 30px">edit</a>
+
+                </div>
+
+                <div class="col-xl-3">
+                  <form action="/posts/{{$post->id}}" method="POST">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-outline-danger" onclick="return confirm('Are you sure ?');" > Delete </button>
+                </form> 
+                </div>
+
+                <div class="col-lg-3">
+
+                </div>
+
+              </div>
+
+              <br>
+              <div class="row">
+                  <div class="col-xl-4">
+                    <h1>Patient Details</h1>
+                    <table border="1px">
+
+                      <tr> <td>NIC number </td> <td>{{$post->NIC_number}}  </td> </tr>
+                      <tr> <td> Age</td> <td> {{$post->Age}} </td> </tr>
+                      <tr> <td> Phone Number</td>  <td>{{$post->phone_number}} </td> </tr>
+                      <tr> <td> Address </td> <td> {{$post->address}} </td> </tr>
+
+                    </table>
+                  </div>
+
+                  <div class="col-xl-4">
+                    <h1>Required Donation Details</h1>
+                    <table border="1px"> 
+                      <tr> <td> Donation Amount</td> <td> {{$post->donation_amount}}</td> </tr>
+                      <tr> <td> Required Date</td> <td> &nbsp; Before in {{$post->required_date}},<br> &nbsp; I need this donation <br> &nbsp; for medical treatment.</td> </tr>
+                    </table>
+                    <br>
+                    <a download class="btn btn-primary" href="patients/{{$post->NIC_number}}/med-document/{{$post->med_report}}">Download medical report</a>
+                  </div>
+
+                  <div class="col-xl-4">
+                    <h1>Patient's Bank Details</h1>
+                    <table border="1px">
+                      <tr> <td>Bank Name </td> <td> {{$post->bank_name}}</td> </tr>
+                      <tr> <td> Bank Account Number </td> <td> {{$post->bank_account_no}}</td> </tr>
+                      <tr> <td>Branch Name </td> <td> {{$post->branch_name}}</td> </tr>
+                    </table>
+                  </div>
+
+              </div>
+         
+    
           
+
+        </center>
+            
+    {{-- </div> --}}
+
+         
        
       <!-- End poster -->
     
