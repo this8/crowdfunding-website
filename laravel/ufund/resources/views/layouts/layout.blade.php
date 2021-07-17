ebb6de0e8f29f30d38e89843ec232f069e312106
Thisura Samarakoon <thissamarakoon@gmail.com>
2021-07-17 21:14:14 +0530

    edit form blade file grammer changes

--- laravel/ufund/resources/views/layouts/layout.blade.php
+++ laravel/ufund/resources/views/layouts/layout.blade.php
@@ -22,11 +22,11 @@
     <header class="l-header" id="header">
         <nav class="nav bd-container">
             <div class="ufund__logo"><img src="/css/images/ufund_logo.png" alt="ufund logo"></div>
-            <a href="#" class="nav__logo">uFund</a>
+            <a href="{{route('home')}}" class="nav__logo">uFund</a>
 
             <div class="nav__menu" id="nav-menu">
                 <ul class="nav__list">
-                    <li class="nav__item"><a href="{{route('home')}}" class="nav__link active-link">Home</a></li>
+                    <li class="nav__item"><a href="/home#home" class="nav__link active-link">Home</a></li>
                     <li class="nav__item"><a href="/home#donate" class="nav__link">Donate</a></li>
                     <li class="nav__item"><a href="/home#categories" class="nav__link">Categories</a></li>
                     <li class="nav__item">
